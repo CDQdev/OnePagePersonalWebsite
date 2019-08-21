36f02ae780a8a353ad991688866fd0d1ba0ff2d2
CDQdev <42492010+CDQdev@users.noreply.github.com>
2019-08-21 18:33:58 +0100

    js file with initail function

--- PWebPage.js
+++ PWebPage.js
@@ -1,1 +1,9 @@
-alert("Testing page!");
+//alert("Testing page!");
+
+//IIFE (Immediately Invoked Function Expression) is a JavaScript function to shield our code from the global scope
+!(function(d){
+  // All code will go in here. We've renamed 'document' to 'd'.
+}(document));
+
+
+
