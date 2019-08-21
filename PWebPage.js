db813bb3cb0e18171860a5169e573ef972b1d9ed
CDQdev <42492010+CDQdev@users.noreply.github.com>
2019-08-21 18:35:57 +0100

    js update
    set classes and event listeners

--- PWebPage.js
+++ PWebPage.js
@@ -2,7 +2,22 @@
 
 //IIFE (Immediately Invoked Function Expression) is a JavaScript function to shield our code from the global scope
 !(function(d){
-  // All code will go in here. We've renamed 'document' to 'd'.
+//set classes
+ function setInitialClasses() {
+  // Targets the previous, current, and next items
+  // This assumes there are at least three items.
+  items[totalItems - 1].classList.add("prev");
+  items[0].classList.add("active");
+  items[1].classList.add("next");
+}
+// Set event listeners
+function setEventListeners() {
+  var next = d.getElementsByClassName('carousel__button--next')[0],
+      prev = d.getElementsByClassName('carousel__button--prev')[0];
+  next.addEventListener('click', moveNext);
+  prev.addEventListener('click', movePrev);
+}
+
 }(document));
 
 
